fc1ced44b87defb5b4ce3f23c7a9e28c271f9b86
Cristian Rios <cristianriosv@gmail.com>
2023-10-11 19:20:18 +0200

    refactor: create constants, clean unused lines and compress lines

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -18,37 +18,39 @@ export class GildedRose {
   }
 
   updateQuality() {
+    const SULFURUS_NAME = 'Sulfuras, Hand of Ragnaros';
+    const AGED_BRIE_NAME = 'Aged Brie';
+    const BACKSTAGE_PASSES_NAME = 'Backstage passes to a TAFKAL80ETC concert';
+    const BACKSTAGE_PASSES_10_DAYS = 10;
+    const BACKSTAGE_PASSES_5_DAYS = 5;
+    const MAXIMUM_QUALITY = 50;
+    const MINIMUM_QUALITY = 0;
+
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
 
       let qualityChangeAmount = -1;
       let sellInChangeAmount = -1;
-      let qualitySellInFactor = 1;
+      let qualitySellInFactor = item.sellIn > 0 ? 1 : 2;
 
-      if (item.sellIn <= 0) {
-        qualitySellInFactor = 2;
-      }
-      if (item.name == 'Sulfuras, Hand of Ragnaros') {
+      if (item.name == SULFURUS_NAME) {
         sellInChangeAmount = 0;
         qualityChangeAmount = 0;
-        qualitySellInFactor = 0;
-      } else if (item.name == 'Aged Brie') {
+      } else if (item.name == AGED_BRIE_NAME) {
         qualityChangeAmount = 1; 
-      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      } else if (item.name == BACKSTAGE_PASSES_NAME) {
         qualityChangeAmount = 1;
-        if (item.sellIn <= 0) {
-          qualityChangeAmount = -item.quality;
-        } else if (item.sellIn <= 5) {
-          qualityChangeAmount = 3;
-        } else if (item.sellIn <= 10) {
-          qualityChangeAmount = 2;
-        }
+        if (item.sellIn <= 0)  qualityChangeAmount = -item.quality;
+        else if (item.sellIn <= BACKSTAGE_PASSES_5_DAYS) qualityChangeAmount = 3;
+        else if (item.sellIn <= BACKSTAGE_PASSES_10_DAYS) qualityChangeAmount = 2;
       }
+
+      const qualityChangeResult = item.quality + qualityChangeAmount * qualitySellInFactor;
       
       if (qualityChangeAmount < 0) {
-        item.quality = Math.max(item.quality + qualityChangeAmount * qualitySellInFactor, 0);
-      } else if (item.quality < 50) {
-        item.quality = Math.min(item.quality + qualityChangeAmount * qualitySellInFactor, 50);
+        item.quality = Math.max(qualityChangeResult, MINIMUM_QUALITY);
+      } else if (item.quality < MAXIMUM_QUALITY) {
+        item.quality = Math.min(qualityChangeResult, MAXIMUM_QUALITY);
       }
 
       item.sellIn += sellInChangeAmount;
