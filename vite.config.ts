6b549a88a4e025283de61535b2e4af8a0aa3c6eb
Cristian Rios <cristianriosv@gmail.com>
2023-10-12 20:54:17 +0200

    feat: add svg support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgr from '@svgr/rollup';
 
 export default defineConfig({
     base: '',
-    plugins: [react(), viteTsconfigPaths()],
+    plugins: [
+        svgr(),
+        react(),
+        viteTsconfigPaths()
+    ],
     server: {    
         open: true,
         port: 3000, 
