880a878536888d5458ce1ca009e4f031a27de915
Cristian Rios <cristianriosv@gmail.com>
2023-10-12 13:10:40 +0200

    feat: implement update next day feature in context

--- src/store/AppContext.ts
+++ src/store/AppContext.ts
@@ -1,5 +1,13 @@
 import { createContext } from "react";
 
-export const AppContext = createContext({
-    gildedRoseInventory: null
+type AppContextStateProps = {
+    gildedRoseInventory: any;
+    currentDate: Date;
+    updateToNextDay: () => void;
+}
+
+export const AppContext = createContext<AppContextStateProps>({
+    gildedRoseInventory: null,
+    currentDate: null,
+    updateToNextDay: () => null
 });
--- src/store/AppProvider.tsx
+++ src/store/AppProvider.tsx
@@ -3,11 +3,11 @@ import { GildedRose } from '@/gilded-rose';
 import { AppContext } from './AppContext';
 
 const INITIAL_ITEMS = [
-    { name: 'Common item 1', sellIn: 10, quality: 20 },
-    { name: 'Common item 2', sellIn: 10, quality: 20 },
-    { name: 'Aged Brie item', sellIn: 10, quality: 20 },
-    { name: 'Sulfurus item', sellIn: 10, quality: 20 },
-    { name: 'Backtage passes to Cristian Rios Concert', sellIn: 10, quality: 20 }
+    { name: 'Common item 1', sellIn: 5, quality: 20 },
+    { name: 'Common item 2', sellIn: 5, quality: 20 },
+    { name: 'Aged Brie item', sellIn: 5, quality: 20 },
+    { name: 'Sulfurus item', sellIn: 5, quality: 20 },
+    { name: 'Backtage passes to Cristian Rios Concert', sellIn: 5, quality: 20 }
 ]
 
 type AppProviderProps = {
@@ -15,10 +15,16 @@ type AppProviderProps = {
 }
 
 const AppProvider = ({ children }: AppProviderProps) => {
-    const [gildedRoseInventory] = useState<GildedRose>(new GildedRose(INITIAL_ITEMS));
+    const gildedRoseInventory = new GildedRose(INITIAL_ITEMS);
+    const [currentDate, setCurrentDate] = useState<Date>(new Date());
+
+    const updateToNextDay = () => {
+        setCurrentDate((currentDate) => new Date(currentDate.setDate(currentDate.getDate() + 1)));
+        gildedRoseInventory.updateQuality();
+    }
 
     return (
-        <AppContext.Provider value={{ gildedRoseInventory }}>
+        <AppContext.Provider value={{ gildedRoseInventory, currentDate, updateToNextDay }}>
             {children}
         </AppContext.Provider>
     );
