80aaedd430ec742d03911c01dd7aa385296bc582
Cristian Rios <cristianriosv@gmail.com>
2023-10-12 13:56:34 +0200

    fix: add correct values to mock data

--- src/store/AppProvider.tsx
+++ src/store/AppProvider.tsx
@@ -3,11 +3,12 @@ import { GildedRose } from '@/gilded-rose';
 import { AppContext } from './AppContext';
 
 const INITIAL_ITEMS = [
-    { name: 'Common item 1', sellIn: 5, quality: 20 },
-    { name: 'Common item 2', sellIn: 5, quality: 20 },
-    { name: 'Aged Brie item', sellIn: 5, quality: 20 },
-    { name: 'Sulfurus item', sellIn: 5, quality: 20 },
-    { name: 'Backtage passes to Cristian Rios Concert', sellIn: 5, quality: 20 }
+    { name: 'Common item 1', sellIn: 10, quality: 20 },
+    { name: 'Common item 2', sellIn: 10, quality: 20 },
+    { name: 'Aged Brie item', sellIn: 10, quality: 20 },
+    { name: 'Sulfuras item', sellIn: 10, quality: 80 },
+    { name: 'Backstage passes to Cristian Rios Concert', sellIn: 15, quality: 20 },
+    { name: 'Conjured item', sellIn: 10, quality: 50 }
 ]
 
 type AppProviderProps = {
