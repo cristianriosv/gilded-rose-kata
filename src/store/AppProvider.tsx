d7db6cb5c9cbfb6ac6f1dbdfdbe043e4e59047a0
Cristian Rios <cristianriosv@gmail.com>
2023-10-12 12:43:35 +0200

    build: add more items to mock data

--- src/store/AppProvider.tsx
+++ src/store/AppProvider.tsx
@@ -3,8 +3,11 @@ import { GildedRose } from '@/gilded-rose';
 import { AppContext } from './AppContext';
 
 const INITIAL_ITEMS = [
-    { name: 'Random item 1', sellIn: 10, quality: 20 },
-    { name: 'Random item 2', sellIn: 10, quality: 20 }
+    { name: 'Common item 1', sellIn: 10, quality: 20 },
+    { name: 'Common item 2', sellIn: 10, quality: 20 },
+    { name: 'Aged Brie item', sellIn: 10, quality: 20 },
+    { name: 'Sulfurus item', sellIn: 10, quality: 20 },
+    { name: 'Backtage passes to Cristian Rios Concert', sellIn: 10, quality: 20 }
 ]
 
 type AppProviderProps = {
